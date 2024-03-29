948c039acc41c16a20ef812e3e15871c09c1a9b2
Махова Анастасия Геннадьевна <PrincePineapple@yandex.ru>
2024-03-29 10:17:26 +0300

    hello. not sin?

--- 4kurs/9/main.cpp
+++ 4kurs/9/main.cpp
@@ -12,17 +12,15 @@ using namespace std;
 
 double lambda_n(int n, int N);
 double y_k_n(int k,int n, int N);
-
+int check_solution(int N, double eps);
 
 //plot '2.txt' using 1:2 with linespoints, '2.txt' using 1:3 with lines
 
 int main(int argc, char* argv[])
 {
     int N = -1; // число узлов
-    double h  = 0;
-    double res =0;
-    int err = 0;
-    //double eps = 0.00000000001;
+    double eps = 1e-10;
+    int res = -1;
 
 
     if(!((argc == 2)&&
@@ -40,25 +38,14 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    h = 1/(double)N;
+    res = check_solution(N, eps);
 
-    for(int n = 0; n < N; n++)
-    {
-        for(int k = 1; i<N;i++)
-        {
-            res = (y_k_n(k+1, n, N)  - 2*y_k_n(k, n, N) + y_k_n(k-1, n, N))/(double)(2*h*h)) + lambda_n(n,N) *y_k_n(k, n, N);
-
-            if(fabs(res) < 1e-10)
-            {
-                cout<<"False"<<endl;
-                err = -1;
-                break;
-            }
-        }
-    }
-    if(err == 0)
+    if(res  == 0)
     {
         cout<<"True"<<endl;
+    }
+    else{
+        cout<<"False"<<endl;
     }
 
     return 0;
@@ -67,12 +54,34 @@ int main(int argc, char* argv[])
 
 double lambda_n(int n, int N)
 {
+    double h = (1/(double)N);
     return 2/((double) h*h)*(1 -cos((2*M_PI*n + M_PI)/((double)(2*N -1))));
 }
 
 double y_k_n(int k,int n, int N)
 {
-    return   ;
+    return  cos((M_PI*k*(2*n - 1))/ ((double)(2*N - 1))) ;
+}
+
+int check_solution(int N, double eps)
+{
+    double h = (1/(double)N);
+    double res = 100;
+
+    for(int n = 0; n < N; n++)
+    {
+        for(int k = 1; k < N; k++)
+        {
+            res = ((y_k_n(k+1, n, N)  - 2*y_k_n(k, n, N) + y_k_n(k-1, n, N))/(double)(2*h*h)) + lambda_n(n,N) *y_k_n(k, n, N);
+
+            if(fabs(res) > eps)
+            {
+                cout<<"k = "<<k<<endl<<"n = "<<n<<endl;
+                return -1;
+            }
+        }
+    }
+    return 0;
 }
 
 
