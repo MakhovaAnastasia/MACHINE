ef73a845747f0f93dd1cdd24bb40fc54cd9ef4b9
Makhova Anastasia <PrincePineapple@yandex.ru>
2024-04-10 11:36:54 +0300

    donegit add .

--- 4kurs/9/main.cpp
+++ 4kurs/9/main.cpp
@@ -19,9 +19,8 @@ int check_solution(int N, double eps);
 int main(int argc, char* argv[])
 {
     int N = -1; // число узлов
-    double eps = 1e-10;
+    double eps = 1e-5;
     int res = -1;
-
 
     if(!((argc == 2)&&
     (sscanf(argv[1],"%d",&N)==1)))
@@ -48,6 +47,26 @@ int main(int argc, char* argv[])
         cout<<"False"<<endl;
     }
 
+/*
+    //проверим скалярное произведение
+    double    h = (1/(double)N);
+    cout<<setprecision(10)<<fixed;
+
+    for(int i = 1; i < N; i++) //k = 1...N-1
+    {
+        for(int j = 1; j < N; j++)
+        {
+            double res = 0;
+            for(int n = 0; n < N; n++ )
+            {
+                 res+=y_k_n(i,n,N)*y_k_n(j,n,N);
+            }
+            cout<<setw(15)<<res*h;
+        }
+        cout<<endl;
+
+    }
+*/
     return 0;
 }
 
@@ -55,12 +74,13 @@ int main(int argc, char* argv[])
 double lambda_n(int n, int N)
 {
     double h = (1/(double)N);
-    return 2/((double) h*h)*(1 -cos((2*M_PI*n + M_PI)/((double)(2*N -1))));
+    return 2*N*N*(1 - cos(M_PI*h*(n + 0.5)));
 }
 
 double y_k_n(int k,int n, int N)
 {
-    return  cos((M_PI*k*(2*n - 1))/ ((double)(2*N - 1))) ;
+    double h = (1/(double)N);
+    return  cos(M_PI*k*h*(n + 0.5)) ;
 }
 
 int check_solution(int N, double eps)
@@ -72,11 +92,12 @@ int check_solution(int N, double eps)
     {
         for(int k = 1; k < N; k++)
         {
-            res = ((y_k_n(k+1, n, N)  - 2*y_k_n(k, n, N) + y_k_n(k-1, n, N))/(double)(2*h*h)) + lambda_n(n,N) *y_k_n(k, n, N);
+            res = ((y_k_n(k+1, n, N)  - 2*y_k_n(k, n, N) + y_k_n(k-1, n, N))/(double)(h*h)) + lambda_n(n,N) *y_k_n(k, n, N);
+
 
             if(fabs(res) > eps)
             {
-                cout<<"k = "<<k<<endl<<"n = "<<n<<endl;
+                cout<<"k = "<<k<<endl<<"n = "<<n<<endl<<"res = "<<res<<endl;
                 return -1;
             }
         }
