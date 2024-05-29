5f46d5aeb267124e5067e426022dae4b649caca4
Махова Анастасия Геннадьевна <PrincePineapple@yandex.ru>
2024-05-29 17:09:59 +0300

    mb&

--- 4kurs/11/main.cpp
+++ 4kurs/11/main.cpp
@@ -17,9 +17,9 @@ int f(double x, double* y, double* z, int M);
 int ans(double x,double* z, int M);
 int NU(double*z, int M);
 void K_1(double x, double* y, double* k1, double h, double M);
-void K_2(double x, double* y,  double* k1,double* k2, double h, double M);
-void K_3(double x, double* y, double* k2, double* k3, double h, double M);
-void K_4(double x, double* y, double* k3, double* k4, double h, double M);
+void K_2(double x, double* y,  double* k1,double* k2, double h, double M, double*tmp);
+void K_3(double x, double* y, double* k2, double* k3, double h, double M, double*tmp);
+void K_4(double x, double* y, double* k3, double* k4, double h, double M, double*tmp);
 void y_n(double*y, double* k1, double* k2, double* k3, double* k4, int M);
 void E(double*z, double* k1, double* k2, double* k3, double* k4, int M);
 void Generate_x(double* MATRIX, double h, int N);
@@ -30,25 +30,25 @@ double normV(double*A,  int M);
 
 int main(int argc, char* argv[])
 {
-    double *y, *x, *k1, *k2, *k3, *k4, *res;
+    double *y, *x, *k1, *k2, *k3, *k4, *res, *tmp;
     double h = -0.1;//timestep
     int N = 0;//num of timesteps
-    int M = 1;//num of equations
+    int M = 2;//num of equations
     if(!((argc == 2)&&
-    (sscanf(argv[1],"%lf",&h)==1)))
+    (sscanf(argv[1],"%d",&N)==1)))
     {
         //ошибка чтения
         return -1;
     }
 
-    if(h<=0)
+    if(N<=0)
     {
         //ошибка 
-        cout<<h<<endl;
-        cout<<" h >= 0"<<endl;
+        cout<<N<<endl;
+        cout<<" N>= 0"<<endl;
         return -1;
     }
-    N = (int)((1.)/h);
+    h = ((M_PI)/(double)N);
 
     y = (double*) malloc(sizeof(double)*(N+1));
     x = (double*) malloc(sizeof(double)*(N+1));
@@ -57,6 +57,7 @@ int main(int argc, char* argv[])
     k3 = (double*) malloc(sizeof(double)*(N+1));
     k4 = (double*) malloc(sizeof(double)*(N+1));
     res = (double*) malloc(sizeof(double)*(N+1));
+    tmp = (double*) malloc(sizeof(double)*(N+1));
 
     Generate_x(x, h, N);
 
@@ -70,9 +71,9 @@ int main(int argc, char* argv[])
         for(int i = 1; i <=N; i++)
         {
             K_1(x[i],y,k1,h,M);
-            K_2(x[i],y,k1,k2,h,M);
-            K_3(x[i],y,k2,k3,h,M);
-            K_4(x[i],y,k3,k4,h,M);
+            K_2(x[i],y,k1,k2,h,M, tmp);
+            K_3(x[i],y,k2,k3,h,M, tmp);
+            K_4(x[i],y,k3,k4,h,M, tmp);
 
             y_n(y,k1,k2,k3,k4,M);
             ans(x[i], res, M);
@@ -100,41 +101,57 @@ int main(int argc, char* argv[])
     free(k3);
     free(k4);
     free(res);
+    free(tmp);
     return 0;
 }
 
 
 int f(double x, double* y, double* z, int M)
 {
-    if(M!= 1)
+    if(M!= 2)
     {return -1;}
     //z[0] = -2*x;
+
     y[0] = y[0];//просто, чтобы использовать y, если он не нужен
     x = x;
 
-    z[0] = y[0]; //exp
+    //z[0] = y[0]; //exp
     //z[0] = 5*x*x*x*x +1;
     //z[1] = 4*x*x*x + 2*x -1;
+    //z[0] = 5*x*x*x*x;
+    //z[1] = 4*x*x*x;
+    z[0] = y[1];
+    z[1] = -y[0];
     return 1;
 }
 
 int ans(double x,double* z, int M)
 {
-    if(M!= 1)
+    if(M!= 2)
     {return -1;}
     //z[0] = -x*x + 1;
-    z[0] = exp(x);
+    //z[0] = exp(x);
+
     //z[0] = x*x*x*x*x + x;
     //z[1] = x*x*x*x + x*x - x;
+
+    //z[0] = x*x*x*x*x;
+    //z[1] = x*x*x*x +1;
+
+    z[0] = sin(x);
+    z[1] = cos(x);
+
+    //z[0] = exp(x);
+    //z[1] = exp(x);
     return 1;
 }
 
 int NU(double*z, int M)
 {
-    if(M!= 1)
+    if(M!= 2)
     {return -1;}
-    z[0] = 1;
-    //z[1] = 0;
+    z[0] = 0;
+    z[1] = 1;
     return 1;
 }
 void K_1(double x, double* y, double* k1, double h, double M)
@@ -146,13 +163,13 @@ void K_1(double x, double* y, double* k1, double h, double M)
     }
     return;
 }
-void K_2(double x, double* y,  double* k1,double* k2, double h, double M)
+void K_2(double x, double* y,  double* k1,double* k2, double h, double M, double* tmp)
 {
     for(int i = 0; i<M; i++)
     {
-        k2[i] = y[i]+0.5*k1[i];
+        tmp[i] = y[i]+0.5*k1[i];
     }
-    f(x+0.5*h, k2, k2, M);
+    f(x+0.5*h, tmp, k2, M);
     for(int i = 0; i<M; i++)
     {
         k2[i] *= h;
@@ -160,13 +177,13 @@ void K_2(double x, double* y,  double* k1,double* k2, double h, double M)
     return;
 }
 
-void K_3(double x, double* y, double* k2, double* k3, double h, double M)
+void K_3(double x, double* y, double* k2, double* k3, double h, double M, double* tmp)
 {
     for(int i = 0; i<M; i++)
     {
-        k3[i] = y[i]+0.5*k2[i];
+        tmp[i] = y[i]+0.5*k2[i];
     }
-    f(x+0.5*h, k3, k3, M);
+    f(x+0.5*h, tmp, k3, M);
     for(int i = 0; i<M; i++)
     {
         k3[i] *= h;
@@ -174,13 +191,13 @@ void K_3(double x, double* y, double* k2, double* k3, double h, double M)
     return;
 }
 
-void K_4(double x, double* y, double* k3, double* k4, double h, double M)
+void K_4(double x, double* y, double* k3, double* k4, double h, double M, double* tmp)
 {
     for(int i = 0; i<M; i++)
     {
-        k4[i] = y[i]+k3[i];
+        tmp[i] = y[i]+k3[i];
     }
-    f(x+h, k4, k4, M);
+    f(x+h, tmp, k4, M);
     for(int i = 0; i<M; i++)
     {
         k4[i] *= h;
